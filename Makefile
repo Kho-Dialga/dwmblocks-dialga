6998513ae8b1da2a464923336f66bd0a8865952b
Kho-Dialga <ivandashenyou@gmail.com>
2021-04-30 07:25:50 -0600

    moved to blocks.h and added instant patch

--- Makefile
+++ Makefile
@@ -5,7 +5,9 @@ CC = gcc
 
 dwmblocks: dwmblocks.o
 	$(CC) dwmblocks.o -lX11 -o dwmblocks
-dwmblocks.o: dwmblocks.c config.h
+blocks.h:
+	cp blocks.def.h $@
+dwmblocks.o: dwmblocks.c blocks.h
 	$(CC) -c dwmblocks.c
 clean:
 	rm -f *.o *.gch dwmblocks
---
+++ blocks.h
@@ -0,0 +1,29 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{"", "cat /tmp/recordingicon 2>/dev/null",		0,		9},
+	{"",	"music",					18000,		11 },
+	{"",	"pacpackages",					0,		8},
+	{"",	"torrent",					20,		7},
+	{"",	"forecast",					300,		5},
+	{"",	"news",						0,		6},
+	{"",	"mailbox",					180,		12 },
+	{"",	"cpu",						1,		18 },
+	{"",	"memory",					1,		14 },
+	{"",	"temp",						1,		18 },
+	{"",	"disk",						180,		13 },
+	{"",	"disk /home",					180,		13 },
+	{"",	"disk $HOME/.local/share",			180,		13 },
+	{"",	"volume",					0,		10 },
+	{"",	"kbselect",					0,		30 },
+	{"",	"clock",					1,		1 },
+	{"",	"internet",					5,		4 },
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = " ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
